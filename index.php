c99069dcaffd6c5b8fc5892bd54d04a0574d6244
MicheleSerafini <serafini.michele01@gmail.com>
2023-11-24 15:13:45 +0100

    ADD > Skeleton static layout to start

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php 
+$title = 'PHP ToDo List JSON';
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?= $title ?></title>
+  <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+  <script src="https://unpkg.com/vue@3"></script>
+  <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+  
+  <div id="app">
+    <div class="container">
+      <div class="flex">
+
+        <div class="tasks-wrapper">
+          <h1 class="title">TODO - List</h1>
+          <input class="input-field" type="text" placeholder="scrivi una task">
+  
+          <ul class="tasks-list">
+            <li class="task">
+              <span class="text">Lorem, ipsum dolor.</span>
+              <span class="delete">&#215;</span>
+            </li>
+          </ul>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+  <script src="./js/app.js"></script>
+</body>
+</html>
---
+++ js/app.js
@@ -0,0 +1,9 @@
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      test: 'Funziona tutto!'
+    }
+  },
+}).mount('#app')
